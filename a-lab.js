e59bd150e8ffc7c47bd5ce43fd4e55fc14521beb
Timothy Lim <contacttimothylim@gmail.com>
2024-10-15 17:44:34 -0400

    commit

---
+++ a-lab.js
@@ -0,0 +1,14 @@
+// exercise 1
+
+const nums = [13, 87, 2, 89, 12, 4, 90, 63];
+const doubledNums = nums.map(num => num * 2);
+console.log(doubledNums); 
+
+
+// exercise 2 
+const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+const [firstTopping, secondTopping] = pizzaToppings;
+console.log(firstTopping); 
+console.log(secondTopping); 
+
+// exercise 3
